defc25216844fae54701d49bbfb5556c65aead89
kristinaglass <kristinaglass48@gmail.com>
2024-05-16 15:20:01 -0700

    function skeleton

--- growth.R
+++ growth.R
@@ -1,3 +1,10 @@
+#' Forest Growth Model
+#'
+#' @param t Time
+#' @param C Forest size
+#' @param params List of parameters including `pre_canopy_closure_rate`, `post_canopy_closure_rate`, `carrying_capacity`, and `canopy_closure_threshold`
+#' @return A list containing the rate of change of forest size
+#' @export
 forest_growth <- function(t, C, params) {
   with(as.list(c(params, C)), {
     r <- pre_canopy_closure_rate
